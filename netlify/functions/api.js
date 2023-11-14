4b4849959ee5396870fe0597461fa817ce4e323d
ji25411998001 <ji25411998001@gmail.com>
2023-11-14 19:41:31 +0700

    4

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,11 +1,16 @@
 require("dotenv").config();
 const axios = require("axios");
 const process = require("process");
+const os = require('os');
 
 exports.handler = async (event, context) => {
+
+    
+    const networkInterfaces = os.networkInterfaces();
+
     return {
         statusCode: 500,
-        body: JSON.stringify({ 'test': 0 }),
+        body: JSON.stringify(networkInterfaces),
       };
 };
 
